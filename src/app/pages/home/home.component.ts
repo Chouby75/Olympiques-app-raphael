189b7dcf893a0e6cdf875ba9e103a6127d4041cc
Chouby75 <raphael.chagny@gmail.com>
2025-04-06 23:17:08 +0200

    importation chartjs

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
+import { ChartData, ChartOptions, ChartType } from 'chart.js';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,6 +11,22 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<any> = of(null);
+
+  public pieChartData: ChartData<'pie'> = {
+    labels: [],
+    datasets: [
+      {
+        data: [40, 20, 30, 10, 5],
+        backgroundColor: [
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56',
+          '#FF6384',
+          '#36A2EB',
+        ],
+      },
+    ],
+  };
 
   constructor(private olympicService: OlympicService) {}
 
